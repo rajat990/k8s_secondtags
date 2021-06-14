2eca403bd45be3ce056d99e95bde02f7d77d79f1
sahil Sachan <rajat.singh990@gmail.com>
2021-06-14 15:00:08 +0530

    thirdupdate

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -1,5 +1,8 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
@@ -7,12 +10,14 @@ import org.springframework.web.bind.annotation.*;
 @EnableAutoConfiguration
 public class Example {
 
-    @Value("${property.value}")
-    String propertyvalue;
+	@Value("${userBucket.path}")
+	private String userBucketPath;
 
-    @RequestMapping(value = "/propertyValue", method = RequestMethod.GET)
-    String propertyvalueMethod(String propertyvalue) {
-        return propertyvalue;
+    @RequestMapping(value = "/property/Value", method = RequestMethod.GET)
+    String propertyvalueMethod() {
+    	
+    	System.out.println(userBucketPath);
+        return userBucketPath;
     }
 
     @RequestMapping(value = "/{msg}", method = RequestMethod.GET)
